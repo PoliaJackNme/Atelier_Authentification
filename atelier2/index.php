da588b27c0f543ddc64995e92fc88a270601814a
PoliaJackNme <marienfoxperol@gmail.com>
2024-12-18 10:55:44 +0100

    Update index.php

--- atelier2/index.php
+++ atelier2/index.php
@@ -1,9 +1,10 @@
 <?php
 
-$cookieuser = bin2hex(random_bytes(16))
+
 // Démarrer une session utilisateur qui sera en mesure de pouvoir gérer les Cookies
 session_start();
 
+$cookieuser = bin2hex(random_bytes(16));
 // Vérifier si l'utilisateur est déjà en possession d'un cookie valide (cookie authToken ayant le contenu 12345)
 // Si l'utilisateur possède déjà ce cookie, il sera redirigé automatiquement vers la page home.php
 // Dans le cas contraire il devra s'identifier.
