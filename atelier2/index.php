c026b79732aaaf5002e6a97af05c7fbc52b288c4
PoliaJackNme <marienfoxperol@gmail.com>
2024-12-18 11:46:48 +0100

    Update index.php

--- atelier2/index.php
+++ atelier2/index.php
@@ -1,45 +1,38 @@
 <?php
-
-
 // Démarrer une session utilisateur qui sera en mesure de pouvoir gérer les Cookies
 session_start();
-
 
 // Vérifier si l'utilisateur est déjà en possession d'un cookie valide (cookie authToken ayant le contenu 12345)
 // Si l'utilisateur possède déjà ce cookie, il sera redirigé automatiquement vers la page home.php
 // Dans le cas contraire il devra s'identifier.
+if (isset($_COOKIE['authToken']) && str_starts_with($_COOKIE['authToken'], 'admin_')) {
+    header('Location: page_admin.php');
+    exit();
+} elseif (isset($_COOKIE['authToken']) && str_starts_with($_COOKIE['authToken'], 'user_')) {
+    header('Location: page_user.php');
+    exit();
+}
 
 // Gérer la soumission du formulaire
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
+
     // Vérification simple du username et de son password.
-    // Si ok alors on initialise le cookie sur le poste de l'utilisateur 
+    // Si ok alors on initialise le cookie sur le poste de l'utilisateur
     if ($username === 'admin' && $password === 'secret') {
-        $cookieadmin = bin2hex(random_bytes(16));
-        setcookie('authToken', $cookieadmin, time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
-        if ('authToken' = $cookieadmin) {
-            header('Location: page_admin.php'); // L'utilisateur est dirigé vers la page home.php
-            exit();
-        } else {
-             $error = "Nom d'utilisateur ou mot de passe incorrect.";
-        }
+        $token = bin2hex(random_bytes(16));
+        setcookie('authToken', 'admin_' . $token, time() + 60, '/', '', true, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes)
+        header('Location: page_admin.php'); // L'utilisateur est dirigé vers la page home.php
+        exit();
+    } elseif ($username === 'user' && $password === 'utilisateur') {
+        $token = bin2hex(random_bytes(16));
+        setcookie('authToken', 'user_' . $token, time() + 60, '/', '', true, true);
+        header('Location: page_user.php');
+        exit();
     } else {
         $error = "Nom d'utilisateur ou mot de passe incorrect.";
     }
-    if ($username === 'utilisateur' && $password === 'utilisateur') {
-        $cookieuser = bin2hex(random_bytes(16));
-        setcookie('authToken', $cookieuser, time() + 60, '/', '', false, true); // Le Cookie est initialisé et valable pendant 1 heure (3600 secondes) 
-        if ('authToken' = $cookieuser) {
-            header('Location: page_user.php'); // L'utilisateur est dirigé vers la page home.php
-            exit();
-        } else {
-             $error = "Nom d'utilisateur ou mot de passe incorrect.";
-        }
-    } else {
-        $error = "Nom d'utilisateur ou mot de passe incorrect.";
-    }
-    
 }
 ?>
 
@@ -63,6 +56,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         <button type="submit">Se connecter</button>
     </form>
     <br>
-    <a href="../index.html">Retour à l'accueil</a>  
+    <a href="../index.html">Retour à l'accueil</a>
 </body>
 </html>
